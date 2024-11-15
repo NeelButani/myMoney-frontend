9603e581210ccf7ace4087720f48915d61c15fd5
Neel <neel.heet.1234@gmail.com>
2024-11-15 17:33:03 +0530

    moved shell and add records outside of shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,8 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './components/loader/loader.component';
 import { CalenderFilterComponent } from './components/calender-filter/calender-filter.component';
 import { FinicialStatsComponent } from './components/finicial-stats/finicial-stats.component';
-import { AddRecordsComponent } from './components/add-records/add-records.component';
-import { ShellComponent } from './components/shell/shell.component';
+import { ShellComponent } from '../shell/shell.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
@@ -15,7 +14,6 @@ import { FooterComponent } from './components/footer/footer.component';
     LoaderComponent,
     CalenderFilterComponent,
     FinicialStatsComponent,
-    AddRecordsComponent,
     ShellComponent,
     HeaderComponent,
     FooterComponent
@@ -27,7 +25,6 @@ import { FooterComponent } from './components/footer/footer.component';
     LoaderComponent,
     CalenderFilterComponent,
     FinicialStatsComponent,
-    AddRecordsComponent
   ]
 })
 export class SharedModule { }
